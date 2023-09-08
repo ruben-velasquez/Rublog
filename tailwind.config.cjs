1bc79cb432b18c3ce72b113f13be74129ddde10c
ruben-velasquez <rualvemo@hotmail.com>
2023-09-08 20:31:43 -0300

    Added custom colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'dark-gray': '#272829',
+				'medium-gray': '#61677A',
+				'light-gray': '#D8D9DA',
+				'light-yellow': '#FFF6E0',
+			},
+		},
 	},
 	plugins: [],
 }
