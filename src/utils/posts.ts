76e6c9c1db86a6f8db2b588d281c2f772505cd5f
ruben-velasquez <rualvemo@hotmail.com>
2023-09-20 19:02:36 -0300

    Añadidas fechas a los posts

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -3,19 +3,19 @@ const posts: Post[] = [
     title: "Título del post 1",
     description: "Descripción del post 1",
     href: "/posts/1",
-    createdAt: new Date("2022-09-20"),
+    createdAt: new Date("2022-9-20"),
   },
   {
     title: "Título del post 2",
     description: "Descripción del post 2",
     href: "/posts/2",
-    createdAt: new Date("2022-09-20"),
+    createdAt: new Date("2022-9-20"),
   },
   {
     title: "Título del post 3",
     description: "Descripción del post 3",
     href: "/posts/3",
-    createdAt: new Date("2022-09-20"),
+    createdAt: new Date("2023-9-9"),
   },
 ];
 
