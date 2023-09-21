86bc655266c0d8e10b60b0a749aa5f4f128563cd
ruben-velasquez <rualvemo@hotmail.com>
2023-09-20 23:02:41 -0300

    Hecha página de posts

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -1,21 +1,24 @@
 const posts: Post[] = [
   {
-    title: "Título del post 1",
-    description: "Descripción del post 1",
-    href: "/posts/1",
-    createdAt: new Date("2022-9-20"),
+    title: "My first post",
+    description: "In this post I talk about my life and what I like to do.",
+    href: "/posts/my-first-post",
+    createdAt: new Date("2023-9-20"),
+    id: 1
   },
   {
     title: "Título del post 2",
     description: "Descripción del post 2",
     href: "/posts/2",
-    createdAt: new Date("2022-9-20"),
+    createdAt: new Date("2023-9-20"),
+    id: 2
   },
   {
     title: "Título del post 3",
     description: "Descripción del post 3",
     href: "/posts/3",
     createdAt: new Date("2023-9-9"),
+    id: 3
   },
 ];
 
@@ -26,4 +29,5 @@ type Post = {
   description: string;
   href: string;
   createdAt: Date;
+  id: number;
 };
