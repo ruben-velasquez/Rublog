6d88d2c493b302934a7925db139cc8f390cb3abf
ruben-velasquez <rualvemo@hotmail.com>
2023-09-21 21:16:56 -0300

    Agregados más posts

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -7,19 +7,19 @@ const posts: Post[] = [
     id: 1
   },
   {
-    title: "Título del post 2",
-    description: "Descripción del post 2",
-    href: "/posts/2",
-    createdAt: new Date("2023-9-20"),
+    title: "My second post",
+    description: "In this post I show yo my recent projects, and related topics.",
+    href: "/posts/my-second-post",
+    createdAt: new Date("2023-9-21"),
     id: 2
   },
   {
-    title: "Título del post 3",
-    description: "Descripción del post 3",
-    href: "/posts/3",
-    createdAt: new Date("2023-9-9"),
+    title: "My third post",
+    description: "In this post I will let you take a look of my workflow",
+    href: "/posts/my-third-post",
+    createdAt: new Date("2023-9-22"),
     id: 3
-  },
+  }
 ];
 
 export default posts;
